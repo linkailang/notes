5f83fc63f837dd06f9f1ae67dc9409eae214e17c
linkailang <2698508226@qq.com>
2026-09-11 22:40:23 +0800

    基础数据类型

--- helloword/src/main.rs
+++ helloword/src/main.rs
@@ -1,11 +1,19 @@
-mod hello_word;
-use hello_word::greet_words;
-
+mod Vecc;
+mod stringg;
+use crate::Vecc::test;
+use crate::stringg::string_out;
 fn main() {
     
     // let a: u32 =1;
     // let b =0.1;
     // println!("Hello, world! {}",a + b);
+    // test();
+    let a = 10;
+    //新的地址块
+    let mut a = 10;
+    let a = 11;
+    println!("{}", a);
+    string_out();
     
-    greet_words();
+    
 }
