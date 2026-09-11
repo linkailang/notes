15f8f23d97248127cfe5ce4e9ab37b6059949816
linkailang <2698508226@qq.com>
2026-09-11 23:19:15 +0800

    更新xmind

---
+++ helloword/src/hashmap.rs
@@ -0,0 +1,6 @@
+fn hashmap() {
+    use std::collections::HashMap;
+    let mut scores = HashMap::new();
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+}
